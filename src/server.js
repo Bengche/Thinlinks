d21126d583260d49594f941603b527cf71df4f3c
Brandy <Bengc102@gmail.com>
2025-11-13 15:09:37 +0100

    2 CORS fix

--- src/server.js
+++ src/server.js
@@ -92,11 +92,13 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
+  optionsSuccessStatus: 204,
+  preflightContinue: false,
 };
 
 const corsMiddleware = cors(corsOptions);
 app.use(corsMiddleware);
-app.options("*", corsMiddleware);
+app.options(/.*/, corsMiddleware);
 
 const PORT = 4003;
 
